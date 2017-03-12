237c6f7c3ca7130db6ed7dd53364aff96aa9e900
jersh <Josh@zarvox.local>
2017-03-11 21:58:30 -0500

    Removed Zip and Convert
    Removed Zip because it was ineffective and resource intensive. Removed
    Convert because it was the same thing as encoding.
    Change to and from to encode and decode.
    Added information well in demo.
    
    NOT COMPATIBLE WITH PREVIOUS VERSIONS

--- README.md
+++ README.md
@@ -2,27 +2,13 @@
 JS text-encoder
 
 ## Usage
-`sangwa.to(value);` 
+`sangwa.encode(value);`
 returns encoded text
 
-`sangwa.from(encodedText);`
+`sangwa.decode(encodedText);`
 returns regular text
 
-`sangwa.compress(value);` 
-returns compressed text
-
-`sangwa.decompress(encodedText);`
-returns decompressed text
-
-`sangwa.zip(value);` 
-returns zipped text
-Note: This is inefficient and takes enourmous amounts of time.
-Max Characters: 10000
-
-`sangwa.unzip(encodedText);`
-returns unzipped text
-
-## Usage
+## Include
 Include in html with
 `<script src='https://raw.githubusercontent.com/dotjersh/sangwa/master/sangwa.min.js'></script>`
 
--- sangwa.js
+++ sangwa.js
@@ -1,3 +1,6 @@
+/* Sangwa Text Encoder */
+/* DotJersh.GitHub.io/sangwa/ */
+
 var sangwa = {
   array: "~!1@2#3$4%5^6&7*8(9)0_-=+  qQwWeErRtTyYuUiIoOpP[{]}\\|aAsSdDfFgGhHjJkKlL;:\"'zZxXcCvVbBnNmM,<.>/?",
   key: function(value){
@@ -16,53 +19,7 @@ var sangwa = {
   }
 }
 
-sangwa.to = function(value){
-  //values
-  value = "A" + value;
-  var result = "";
-  var randFiller ="";
-  var randSqnc = ["#","A","@","&","t"];
-  var r = 0;
-
-  var d = Math.floor(Math.random() * 50);
-
-  //convert into ASCII
-  for (var i = 0; i < value.length; i++) {
-      r = Math.floor(Math.random() * 5);
-      randFiller = randSqnc[r];
-      result += value[i].charCodeAt(0) + d + "" + randFiller;
-  }
-
-  //publish result
-  return result;
-}
-
-sangwa.from = function(value){
-  //values
-  var result = "";
-  var randFiller ="";
-  var randSqnc = ["#","A","@","&","t"];
-  var r = 0;
-
-  value = value.replace(/#/g, ".");
-  value = value.replace(/A/g, ".");
-  value = value.replace(/@/g, ".");
-  value = value.replace(/&/g, ".");
-  value = value.replace(/t/g, ".");
-
-  value = value.split('.');
-
-  var x = parseInt(value[0]) - 65;
-
-  //convert into ASCII
-  for (var i = 1; i < value.length-1; i++) {
-      result += String.fromCharCode(parseInt(value[i]) - x);
-  }
-
-  return result;
-}
-
-sangwa.compress = function(value){
+sangwa.encode = function(value){
   var result = "";
 
   var rand = Math.floor(Math.random() * 6) + 2;
@@ -76,7 +33,7 @@ sangwa.compress = function(value){
   return result;
 }
 
-sangwa.decompress = function(value){
+sangwa.decode = function(value){
   var result = "";
 
   var rand = parseInt(sangwa.key(value[0]) - sangwa.key("A"));
@@ -84,66 +41,6 @@ sangwa.decompress = function(value){
   for(var i = 1; i < value.length; i++){
     result += sangwa.key(sangwa.key(value[i]) - rand);
   }
-
-  return result;
-}
-
-sangwa.zip = function(value){
-  var result = sangwa.compress(value);
-  var keys = "";
-  var hash = 0;
-
-  for(var i = 1; i < result.length; i++){
-    for(var i2 = i + 2; i2 < result.length; i2++){
-      for(var u = 50; u >= 10;u--){
-        var first = result.substring(i,i + u);
-        var second = result.substring(i2,i2 + u);
-
-        if(first == second && first.indexOf("`") < 0){
-          while(result.indexOf(first) >= 0){
-            result = result.replace(first,"`" + hash + "`");
-          }
-
-          keys = hash + "`" + first + "`" + keys;
-
-          hash++;
-        }
-      }
-    }
-  }
-
-  result = keys + "¡" + result
-
-  return result;
-}
-
-function cArray(array){
-  var newarray = new Array();
-
-  for(var i = 0; i < array.length; i++){
-    if(array[i]){
-      newarray.push(array[i]);
-    }
-  }
-
-  return newarray;
-}
-
-sangwa.unzip = function(value){
-  value = value.split("¡");
-
-  var result = value[1];
-  var keys = value[0];
-
-  keys = cArray(keys.split("`"));
-
-  for(var i = 0; i < keys.length; i = i + 2){
-    while(result.indexOf("`" + keys[i] + "`") >= 0){
-      result = result.replace("`" + keys[i] + "`",keys[i+1]);
-    }
-  }
-
-  result = sangwa.decompress(result);
 
   return result;
 }
